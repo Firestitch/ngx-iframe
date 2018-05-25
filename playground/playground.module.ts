3b9da07da353fca0ab0f84ca3bb1ddaf89511d2b
Svyatoslav Gerasimov <s.gerasimov@wis.eu.com>
2018-05-25 17:16:42 +0300

    fix(GO-T715): pulled the latest tools submodule; updated to the latest @firestitch/example; added "filemanager-webpack-plugin"; updated "package.json"; updated structure of Playground

--- playground/playground.module.ts
+++ playground/playground.module.ts
@@ -1,6 +1,5 @@
 import './../tools/assets/playground.scss';
 
-import { RouterModule, Routes } from '@angular/router'
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app/app.component';
@@ -9,8 +8,6 @@ import { FsIFrameModule } from '../src';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app/material.module';
 import { FsExampleModule } from '@firestitch/example';
-import { FsExamplesComponent } from '../tools/components/examples/examples.component';
-import { ExamplesComponent } from './app/components/examples/examples.component';
 import { FirstExampleComponent } from './app/components/first-example/first-example.component';
 import { IFrameEmitterComponent } from './app/components/iframe-emitter/ifame-emitter.component';
 
@@ -24,18 +21,12 @@ import { IFrameEmitterComponent } from './app/components/iframe-emitter/ifame-em
     AppMaterialModule,
     FormsModule,
     FsExampleModule,
-    RouterModule.forRoot([
-      { path: '', component: ExamplesComponent, pathMatch: 'full' },
-      { path: 'emitter', component: IFrameEmitterComponent },
-    ])
   ],
   entryComponents: [
   ],
   declarations: [
     AppComponent,
     FirstExampleComponent,
-    ExamplesComponent,
-    FsExamplesComponent,
     IFrameEmitterComponent
   ],
   providers: [
